ace49cb987e77942857f187d7a841ec361997281
Andrej <andrejtodoroski@yahoo.com>
2024-05-21 15:37:52 -0400

    seeding in progress

--- server/models.py
+++ server/models.py
@@ -30,6 +30,7 @@ class Product(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String, nullable=False)
+    product_img = db.Column(db.String, nullable=False)
     category = db.Column(db.String, nullable=False)
     # is_sold_out = db.Column(db.Boolean, nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
