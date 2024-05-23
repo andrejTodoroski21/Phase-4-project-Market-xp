ee5cce05cdbd36f211a8b94b7354cd17f5d12660
Andrej <andrejtodoroski@yahoo.com>
2024-05-23 11:57:19 -0400

    added get listings and more, also updated the tables

--- server/seed.py
+++ server/seed.py
@@ -28,12 +28,12 @@ if __name__ == '__main__':
         will = User(is_seller=False, first_name="Will", last_name="Metzler", username = "Willymet", address="23 blanch ct")
         users = [will, dan, andrej]
 
-        print("creating Products")
-        nintendo_64 = Item(item_name = "N64", item_img = "https://i5.walmartimages.com/seo/Restored-Nintendo-64-N64-System-Video-Game-Console-Refurbished_4d39d2ad-33a4-46df-95d8-c68110826631_2.ed4f595c99d114de02b77d9fdf70d57f.jpeg", category = "console", price = 120, inventory = 2)
-        nintendo_3ds = Item(item_name = "3ds", item_img = "https://cdn.mos.cms.futurecdn.net/madpibo7RKfTtQ3mnaVdPT.jpg", category = "console", price = 80, inventory = 1)
-        eevee_plushie = Item(item_name = "Eevee plushie", item_img = "https://m.media-amazon.com/images/I/71ktlCcangL.jpg", category = "doll", price = 30, inventory = 3)
-        cd_player= Item(item_name = "Sony Walkman", item_img = "https://i.ebayimg.com/images/g/xcAAAOSwr5tg6PCD/s-l1200.webp", category = "", price = 70, inventory = 5)
-        carhartt_jacket = Item(item_name = "Carhartt Jacket", item_img = "https://i.ebayimg.com/images/g/BcUAAOSw2tBj6rHq/s-l1200.webp", category = "", price = 110, inventory = 8)
+        print("creating Items")
+        nintendo_64 = Item(item_name = "N64", item_img = "https://i5.walmartimages.com/seo/Restored-Nintendo-64-N64-System-Video-Game-Console-Refurbished_4d39d2ad-33a4-46df-95d8-c68110826631_2.ed4f595c99d114de02b77d9fdf70d57f.jpeg", category = "console", description ="a super old ancient console where you can play some of the oldest games ever made", price = 120, inventory = 2)
+        nintendo_3ds = Item(item_name = "3ds", item_img = "https://cdn.mos.cms.futurecdn.net/madpibo7RKfTtQ3mnaVdPT.jpg", category = "console", description = "a fun console to play on that has a 3d setting",price = 80, inventory = 1)
+        eevee_plushie = Item(item_name = "Eevee plushie", item_img = "https://m.media-amazon.com/images/I/71ktlCcangL.jpg", category = "doll", description = "a perfect little pokemon toy for a loved one",price = 30, inventory = 3)
+        cd_player= Item(item_name = "Sony Walkman", item_img = "https://i.ebayimg.com/images/g/xcAAAOSwr5tg6PCD/s-l1200.webp", category = "electronics",description = "classic walkman player if youre looking to collect a relic", price = 70, inventory = 5)
+        carhartt_jacket = Item(item_name = "Carhartt Jacket", item_img = "https://i.ebayimg.com/images/g/BcUAAOSw2tBj6rHq/s-l1200.webp", category = "clothing", description = "a popular reliable jacket looks better the more worn out it is",price = 110, inventory = 8)
         items = [nintendo_3ds, nintendo_64, eevee_plushie, cd_player, carhartt_jacket]
         db.session.add_all(orders)
         db.session.add_all(users)
