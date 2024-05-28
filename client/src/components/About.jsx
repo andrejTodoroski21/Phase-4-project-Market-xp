b9e875d289321d44a06228cc412f4209499321e6
Will Metzler <metzlerwill@gmail.com>
2024-05-28 11:55:55 -0400

    profile and about

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,8 +2,26 @@
 function About() {
     return (
         <>
-            <h1>About</h1>
-            <h2>Here is some text about our site...</h2>
+            <br></br>
+            <br></br>
+            <div style={{marginLeft: '30px'}}>
+                    <div style={{width: '30%'}} id="title-window" className="window">
+                    <div style={{height: '30px'}} className="title-bar">
+                    <h3 class="title-bar-text">About</h3>
+
+                    <div class="title-bar-controls">
+                        <button aria-label="Minimize"></button>
+                        <button aria-label="Maximize"></button>
+                        <button aria-label="Close"></button>
+                    </div>
+
+                    </div>
+                    <div class="window-body">
+                        <p>blah blah blah</p>
+                    </div>
+
+                </div>
+            </div>
         </>
     )
 }
