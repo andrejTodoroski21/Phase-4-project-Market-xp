0edab0b9ed1cfb73be9129b029ae90e8b543ba2c
Will Metzler <metzlerwill@gmail.com>
2024-05-28 11:55:55 -0400

    profile and about

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -2,6 +2,26 @@
 function Profile() {
     return (
         <>
+            <br></br>
+            <br></br>
+            <div style={{marginLeft: '30px'}}>
+                    <div style={{width: '30%'}} id="title-window" className="window">
+                    <div style={{height: '30px'}} className="title-bar">
+                    <h3 class="title-bar-text">Profile</h3>
+
+                    <div class="title-bar-controls">
+                        <button aria-label="Minimize"></button>
+                        <button aria-label="Maximize"></button>
+                        <button aria-label="Close"></button>
+                    </div>
+
+                    </div>
+                    <div class="window-body">
+                        <p>blah blah blah</p>
+                    </div>
+
+                </div>
+            </div>
         </>
     )
 }
