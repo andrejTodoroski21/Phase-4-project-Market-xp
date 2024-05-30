d393300420798d719fc272d6890949026bbbac2c
Andrej <andrejtodoroski@yahoo.com>
2024-05-30 14:21:01 -0400

    added comments to code

--- server/app.py
+++ server/app.py
@@ -23,6 +23,7 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+# this route is used to get the users from the database
 @app.get('/api/users')
 def index():
     return [u.to_dict() for u in User.query.all()], 200
@@ -35,11 +36,12 @@ def users_by_id(id):
     else:
         return {'error': 'Not found'}, 404
 
-# USER SIGNUP 
+# we are creating a new user by creating a post request requiring a username, first name, last name, and a password.
 @app.post('/api/users')
 def create_user():
     try:
         new_user = User(username=request.json['username'], first_name=request.json['first_name'], last_name=request.json['last_name'])
+        # using the bcrypt library to hash the password
         new_user._hashed_password = bcrypt.generate_password_hash(request.json['_hashed_password']).decode('utf-8')
         db.session.add(new_user)
         db.session.commit()
@@ -59,32 +61,39 @@ def get_session():
     return {}, 204
 
 # LOGIN AND LOGOUT
+# to login we are creating a post request that requires a username and a password.
 @app.post('/api/login')
 def login():
     username = request.json.get('user')
     password = request.json.get('password')
     user = User.query.filter_by(username=username).first()
+    # in the if statment we are checking to see it the username is in the database and we are hashing the password the user input and 
+    # comparing it to the password in the database.
     if user and bcrypt.check_password_hash(user._hashed_password, password):
         session['user_id'] = user.id  # Store the user ID in the session
         return user.to_dict(), 201
     else:
         return {'error': 'Username or password was invalid'}, 401
 
+# to logout we are creating a delete request by doing session.pop to remove the user id from the session.
 @app.delete('/api/logout')
 def logout():
     session.pop('user_id')  # Remove the user ID from the session on logout
     return {}, 204
 
 # GET LISTINGS
+# we are creating a get request to get the items from the database.
 @app.get('/api/items')
 def get_items():
     return [i.to_dict() for i in Item.query.all()], 200
 
 # POST LISTINGS
+# we are using a post request to create a new item in the database.
 @app.post('/api/items')
 def post_items():
     try:
         data = request.json
+        # (**data) unpacks the data dictionary into the new_listing variable.
         new_listing = Item(**data)
         db.session.add(new_listing)
         db.session.commit()
@@ -92,37 +101,13 @@ def post_items():
     except Exception as e:
         return jsonify( {'error': str(e)} ), 406
 
-@app.patch('/api/items/<int:id>')
-def patch_items(id):
-        items = db.session.query(Item.id == id).first()
-        if items:
-            items.item_name = request.json.get('title')
-            items.description = request.json.get('description')
-            items.price = request.json.get('price')
-        else:
-            return {'error': 'Not found'}, 404
-
-
-# DELETE AND PATCH
-@app.delete('/api/items/<int:id>')
-def delete_items(id):
-    try:
-        user = User.query.where(User.id == session.get('user_id')).first()
-        item = Item.query.where(Item.id == id).first()
-        if item.seller_id == user.id:
-            db.session.delete(item)
-            db.session.commit()
-            return jsonify({'message':'your item has been deleted'}), 204
-        else:
-            return {'error': 'you are not authorized'}, 404
-    except Exception as e:
-        return jsonify( {'error': str(e)} ), 406
-
 # GET COMMENTS
+# here we are creating a get request to get the comments from the database.
 @app.get('/api/comments')
 def get_comments():
     return [c.to_dict() for c in Comment.query.all()], 200
 
+# in this get request we are gettting the comments by the item_id from the database.
 @app.get('/api/items/<int:item_id>/comments')
 def get_comments_by_item_id(item_id):
     item = Item.query.get(item_id)
@@ -133,6 +118,7 @@ def get_comments_by_item_id(item_id):
         return {'error': 'Item not found'}, 404
 
 # POST COMMENTS
+# this post request checks for the current user that is logged in and if the user is authorized to post a comment.
 @app.post('/api/comments')
 def post_comments():
     try:
@@ -145,6 +131,7 @@ def post_comments():
     except Exception as e:
         return jsonify( {'error': str(e)} ), 406
 
+# users can delete their own comments
 @app.delete('/api/comments/<int:id>')
 def delete_comments(id):
     try:
@@ -161,29 +148,7 @@ def delete_comments(id):
     except Exception as e:
         return {'error': str(e)}, 406
 
-@app.post('/api/cart')
-def add_to_cart():
-    try:
-        user_id = session.get('user_id')
-        if not user_id:
-            return jsonify({'error': 'User not logged in'}), 401
-        
-        data = request.json
-        item_id = data['item_id']
-        quantity = data.get('quantity', 1)
-
-        cart_item = Cart.query.filter_by(user_id=user_id, item_id=item_id).first()
-        if cart_item:
-            cart_item.quantity += quantity
-        else:
-            cart_item = Cart(user_id=user_id, item_id=item_id, quantity=quantity)
-            db.session.add(cart_item)
-        
-        db.session.commit()
-        return jsonify(cart_item.to_dict()), 201
-    except Exception as e:
-        return jsonify({'error': str(e)}), 406
-
+# getting the items 
 @app.get('/api/cart')
 def get_cart():
     try:
@@ -196,23 +161,7 @@ def get_cart():
     except Exception as e:
         return jsonify({'error': str(e)}), 406
 
-@app.delete('/api/cart/<int:item_id>')
-def remove_from_cart(item_id):
-    try:
-        user_id = session.get('user_id')
-        if not user_id:
-            return jsonify({'error': 'User not logged in'}), 401
-
-        cart_item = Cart.query.filter_by(user_id=user_id, item_id=item_id).first()
-        if cart_item:
-            db.session.delete(cart_item)
-            db.session.commit()
-            return {}, 204
-        else:
-            return jsonify({'error': 'Item not found in cart'}), 404
-    except Exception as e:
-        return jsonify({'error': str(e)}), 406
-
+# checks if the inventory is greater than 0 and if it is it adds it to the order history
 @app.post('/api/items/<int:id>/buy')
 def buy_now(id):
     item = Item.query.get(id)
@@ -249,6 +198,7 @@ def buy_now(id):
     db.session.commit()
 
     return jsonify({'message': 'Item purchased successfully'}), 200
+    
 
 @app.get('/api/profile')
 def get_purchased_items():
